e86115f03d033a61b1a789077a09d84deebcadf1
Peter Tseng <pht24@cornell.edu>
2020-01-14 04:06:09 +0000

    day 06: only search once
    Faster because doesn't recompute depths a bunch of times.

--- bin/06_universal_orbit_map.hs
+++ bin/06_universal_orbit_map.hs
@@ -2,29 +2,24 @@ import AdventOfCode (readInputFile)
 import AdventOfCode.Search (bfs)
 import AdventOfCode.Split (splitOnOne)
 
-import Data.Map (Map)
+import Data.Either (rights)
 import qualified Data.Map as Map
 import Data.Tuple (swap)
 
 tup2ls :: (a, a) -> [(a, [a])]
 tup2ls (a, b) = [(b, [a]), (a, [b])]
 
-depth :: Map String String -> String -> Int
-depth orbit x = case Map.lookup x orbit of
-  Nothing -> 0
-  Just y -> 1 + depth orbit y
-
 main :: IO ()
 main = do
   s <- readInputFile
   let orbit = map (swap . splitOnOne ')') (lines s)
       transfer = Map.fromListWith (++) (concatMap tup2ls orbit)
+      neigh = flip (Map.findWithDefault []) transfer
 
-  print (sum (map (depth (Map.fromList orbit) . fst) orbit))
+  print . sum . map fst . rights $ bfs neigh (const True) "COM"
 
   let printGoal (Right (gen, _):_) = print gen
       printGoal _ = putStrLn "impossible"
-      neigh = flip (Map.findWithDefault []) transfer
 
   case (lookup "YOU" orbit, lookup "SAN" orbit) of
     (Just youOrbit, Just sanOrbit) -> printGoal (bfs neigh (== sanOrbit) youOrbit)
