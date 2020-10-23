8d6cf16e3cd3f3d22a255b91dd8fdb4cb542f54f
Peter Tseng <pht24@cornell.edu>
2020-10-23 11:41:10 +0000

    day 16: skip unnecessary digits
    cuts runtime to about 0.7x

--- bin/16_flawed_frequency_transmission.hs
+++ bin/16_flawed_frequency_transmission.hs
@@ -1,12 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import AdventOfCode (readInputFile)
 
 import Control.Arrow (second)
-import Control.Monad (when)
 import Control.Monad.ST (ST, runST)
 import Data.Array.MArray (getElems, modifyArray, newArray)
 import Data.Array.ST (STUArray)
 import Data.Array.Unboxed ((!), UArray, bounds, elems, indices, listArray)
-import Data.Bits ((.&.))
 import Data.Char (digitToInt, intToDigit)
 import Data.Foldable (for_)
 import Data.List (dropWhileEnd)
@@ -28,27 +28,22 @@ sumLeftArr arr = listArray (second succ (bounds arr)) (scanl (+) 0 (elems arr))
 snd3 :: (a, b, c) -> b
 snd3 (_, x, _) = x
 
-binom99Mod10 :: Int -> Int
-binom99Mod10 i = (b2 * 5 + b5 * 6) `mod` 10
-  where b2 = if (i + 99) .&. 99 == 99 then 1 else 0
-        b5 = case i `mod` 125 of
-             0 -> 1
-             25 -> 4
-             _ -> 0
-
 fft2 :: UArray Int Int -> Int -> [Int]
 fft2 digits offset = runST $ do
   let (_, len0) = second succ (bounds digits)
       len10k = len0 * 10000 - offset
   a <- newArray (0, 7) 0 :: ST s (STUArray s Int Int)
-  for_ [0 .. (len10k - 1)] $ \i -> do
-    let distFromEnd = len10k - i
-        n = min 8 distFromEnd
-        bin = binom99Mod10 i
-    when (bin /= 0) $
-      for_ [0 .. n - 1] $ \j -> do
-        let idx = (offset + i + j) `mod` len0
-        modifyArray a j (+ (bin * digits ! idx))
+  let stride bigStride littleStrides = do
+        for_ [0, bigStride .. (len10k - 1)] $ \bigStrideBase -> do
+          for_ littleStrides $ \(littleStride, coeff) -> do
+            let i = bigStrideBase + littleStride
+                distFromEnd = len10k - i
+                n = min 8 distFromEnd
+            for_ [0 .. n - 1] $ \j -> do
+              let idx = (offset + i + j) `mod` len0
+              modifyArray a j (+ (coeff * digits ! idx))
+  stride 128 [(x, 5) | x <- [0, 4 .. 28]]
+  stride 125 [(0, 6), (25, 4)]
   es <- getElems a
   return (map (`mod` 10) es)
 
